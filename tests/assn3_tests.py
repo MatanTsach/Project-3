4a4cc58d66ae6cda56b2ef2e846e2df2f841ee8d
Matan Tsach <matan.tsach@gmail.com>
2023-07-19 13:32:08 +0300

    Changes

---
+++ tests/assn3_tests.py
@@ -0,0 +1,23 @@
+import pytest
+import requests
+
+BASE_URL = "http://127.0.0.1"
+
+def test_1():
+    endpoint = f"{BASE_URL}/dishes"
+
+    dishes = ["orange", "spaghetti", "apple pie"]
+    response_ids = []
+
+    for dish in dishes:
+        payload = {"name": dish}
+        response = requests.post(endpoint, json=payload)
+
+        assert response.status_code == 201, "TEST1: Failed, status code incorrect."
+
+        dish_id = response.text.strip()
+
+        assert dish_id not in response_ids, "TEST1: Failed, duplicated dish id"
+        response_ids.append(dish_id)
+
+    print("TEST1: Success")
