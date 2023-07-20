28e8dff7bde043d18ff787e3959207e4a4f5cfe6
Matan Tsach <matan.tsach@gmail.com>
2023-07-20 10:46:02 +0300

    test31

---
+++ QueryExecuter.py
@@ -0,0 +1,22 @@
+import requests
+
+def post_request(url, data):
+    response = requests.post(url, json=data)
+    return response
+
+def get_request(url):
+    response = requests.get(url)
+    return response
+
+def main():
+    endpoint = "http://127.0.0.1/dishes"
+    with open("query.txt", "r") as query_file:
+        for name in query_file:
+            data = {"name": name}
+            response = post_request(endpoint, data)
+            response = get_request(endpoint + '/' + response.json()).json()
+            with open("response.txt", "a") as results_file:
+                results_file.write(f"{name} contains {response['cal']} calories, {response['sodium']} mgs of sodium, and {response['sugar']} grams of sugar\n")
+
+if __name__ == "__main__":
+    main()
